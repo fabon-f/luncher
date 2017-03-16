6e7b64f60369621592b4f812c9fa777bd4e504a9
hinamiyagk <syobon.hinata.public@gmail.com>
2017-03-16 12:06:51 +0900

    Create a simple Electron project

---
+++ index.js
@@ -0,0 +1,33 @@
+const electron = require("electron");
+const { app, BrowserWindow } = electron;
+const path = require("path");
+const url = require("url");
+
+let mainWindow = null;
+
+const createWindow = () => {
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize;
+  mainWindow = new BrowserWindow({ width, height });
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, "index.html"),
+    protocol: "file:",
+    slashes: true
+  }));
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
